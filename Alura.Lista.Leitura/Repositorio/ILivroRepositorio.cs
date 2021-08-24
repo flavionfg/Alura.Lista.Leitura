1a1773c7a0eda7a830c9aa93b6aeaad62ba6ffba
Flavio Quadros <Flavio.Quadros@csgi.com>
2021-08-24 11:18:28 -0300

    First commit

---
+++ Alura.Lista.Leitura/Repositorio/ILivroRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Alura.Lista.Leitura.Negocio;
+
+namespace Alura.Lista.Leitura.Repositorio
+{
+    interface ILivroRepositorio
+    {
+        ListaDeLeitura ParaLer { get; }
+        ListaDeLeitura Lendo { get; }
+        ListaDeLeitura Lidos { get; }
+        IEnumerable<Livro> Todos { get; }
+        void Incluir(Livro livro);
+    }
+}
