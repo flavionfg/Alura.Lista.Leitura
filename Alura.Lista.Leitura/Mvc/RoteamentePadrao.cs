71839ab6da4822b17a6daa21c1e26b21df524ddd
Flavio Quadros <Flavio.Quadros@csgi.com>
2021-09-10 22:32:52 -0300

    Criando a classe roteamento padrão.

---
+++ Alura.Lista.Leitura/Mvc/RoteamentePadrao.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace Alura.Lista.Leitura.Mvc
+{
+    public class RoteamentePadrao
+    {
+        public static Task TratamentoPadrao(HttpContext context)
+        {
+            //rota padrão : /<Classe>Logica/Metodo
+            //{classe}/{metodo}
+
+            var classe = Convert.ToString(context.GetRouteValue("classe"));
+            var nameMetodo = Convert.ToString(context.GetRouteValue("metodo"));
+
+            var nomeCompleto = $"Alura.Lista.Leitura.App.Logica.{classe}Logica";
+
+            var tipo = Type.GetType(classe);
+            var metodo = tipo.GetMethods().Where(m => m.Name == nameMetodo).First();
+            var requestDelegate = (RequestDelegate)Delegate.CreateDelegate(typeof(RequestDelegate), metodo);
+
+            return requestDelegate.Invoke(context);
+        }
+    }
+}
